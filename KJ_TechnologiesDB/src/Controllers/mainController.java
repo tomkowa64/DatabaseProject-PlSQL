850083acccbec54cf107e7cff11afcb5e6ba4a77
Tomasz Kowalski <tomkowa64@gmail.com>
2021-05-23 13:45:42 +0200

    Main View Bar Chart

--- KJ_TechnologiesDB/src/Controllers/mainController.java
+++ KJ_TechnologiesDB/src/Controllers/mainController.java
@@ -5,8 +5,24 @@
  */
 package Controllers;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +30,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -45,7 +65,13 @@ public class mainController implements Initializable{
     private Button exit;
 
     @FXML
-    private AreaChart<?, ?> completedOrdersChart;
+    private BarChart<String, Number> completedOrdersChart;
+    
+    @FXML
+    private CategoryAxis xAxis;
+    
+    @FXML
+    private NumberAxis yAxis;
     
     @FXML
     private void handleExitButtonAction (ActionEvent exitEvent) throws Exception{
@@ -95,11 +121,118 @@ public class mainController implements Initializable{
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-    }    
+    }
+
+    public void getDataChart() throws SQLException, URISyntaxException, MalformedURLException{
+        completedOrdersChart.setTitle("Zamówienia");
+        xAxis.setLabel("Miesiac");
+        yAxis.setLabel("Ilosc zamowien");
+        
+        completedOrdersChart.getStylesheets().add(
+                getClass().getResource("/css/unpad-chart.css").toURI().toURL().toExternalForm()
+        );
+
+        try 
+        {
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+        } 
+        catch (ClassNotFoundException e) {
+                e.printStackTrace();
+        }
+
+        Connection con=DriverManager.getConnection(  
+            "jdbc:oracle:thin:@localhost:1521:xe","system","oracle");  
+        
+        String query = "begin ? := GETMONTH(); end;";
+        
+        CallableStatement stmt = con.prepareCall(query);
+        stmt.registerOutParameter(1, Types.INTEGER);
+        stmt.execute();
+        
+        int currMonth = stmt.getInt(1);
+        
+
+        ArrayList<Integer> monthListInt = new ArrayList<Integer>();
+        ArrayList<Integer> orderListInt = new ArrayList<Integer>();
+        
+        for(int i = currMonth; i > 0; ){
+            monthListInt.add(i);
+            
+            String query2 = "begin ? := getOrderForMonthCount(?); end;";
+
+            CallableStatement stmt2 = con.prepareCall(query2);
+            stmt2.registerOutParameter(1, Types.INTEGER);
+            stmt2.setInt(2, i);
+            stmt2.execute();
+            orderListInt.add(stmt2.getInt(1));
+            i--;
+        }
+        
+        Collections.reverse(orderListInt);
+        Collections.reverse(monthListInt);
+        
+        Map map = new HashMap();
+ 
+        map.put(-5, "Lipiec");        
+        map.put(-4, "Sierpień");        
+        map.put(-3, "Wrzesień");        
+        map.put(-2, "Październik");       
+        map.put(-1, "Listopad");        
+        map.put(0, "Grudzień");
+        map.put(1, "Styczen");
+        map.put(2, "Luty");
+        map.put(3, "Marzec");
+        map.put(4, "Kwiecen");
+        map.put(5, "Maj");
+        map.put(6, "Czerwiec");
+        map.put(7, "Lipiec");
+        map.put(8, "Sierpien");
+        map.put(9, "Wrzesien");
+        map.put(10, "Pazdziernik");
+        map.put(11, "Listopad");
+        map.put(12, "Grudzien");
+      
+        
+        Map map2 = new HashMap();
+        map2.put(1,1);
+        map2.put(2,2);
+        map2.put(3,3);
+        map2.put(4,4);
+        map2.put(5,5);
+        map2.put(6,6);
+        map2.put(7,7);
+        map2.put(8,8);
+        map2.put(9,9);
+        map2.put(10,10);
+        map2.put(11,11);
+        map2.put(0,12);
+        map2.put(-1,11);
+        map2.put(-2,10);
+        map2.put(-3,9);
+        map2.put(-4,8);
+        map2.put(-5,7);
+        
+        XYChart.Series<String,Number> seriesOrders = new XYChart.Series();
+        seriesOrders.setName("Zamowienia");
+        
+        for(int i = 0; i < monthListInt.size(); i++){
+            seriesOrders.getData().add(new XYChart.Data(monthListInt.get(i).toString(), orderListInt.get(i)));
+        }
+        completedOrdersChart.getData().add(seriesOrders);
+        con.close();
+    } 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        try {
+            getDataChart();
+        } catch (SQLException ex) {
+            Logger.getLogger(mainController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(mainController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(mainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
